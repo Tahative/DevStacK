648804379ebf0d7257fdc24bab43dc3d9bea28a2
Tahative <tahaminabd133199722@gmail.com>
2026-09-13 23:41:14 +0600

    logo fix

--- src/Components/AvailableStacks.tsx
+++ src/Components/AvailableStacks.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+type Stack = {
+  id: string 
+  name: string;
+};
 
-const AvailableStacks = ({stacks}) => {
-    console.log();
-    return (
-        <div>
-            
-        </div>
-    );
+type AvailableStacksProps = {
+  stacks: Stack[];
+};
+
+const AvailableStacks = ({ stacks }: AvailableStacksProps) => {
+  console.log(stacks, "available stacks");
+
+  return (
+    <div>
+      {stacks.map((stack) => {
+        return (
+          <div key={stack.id}>
+            <h2>{stack.name}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AvailableStacks;
